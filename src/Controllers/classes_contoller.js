9e9bdc0b4a8deedd0464372f888bad02ef2625c4
unknown <deepanshugulia256@gmail.com>
2022-04-17 20:04:29 +0530

    Backend part is done and ready to depoy

--- src/Controllers/classes_contoller.js
+++ src/Controllers/classes_contoller.js
@@ -10,23 +10,36 @@ router.post("", async (req, res) => {
 
     console.log("Class added");
 
-    res.status(202).send(user);
+    res.status(202).send({ error: false, class: user });
   } catch (error) {
     console.log("ERROR:", error);
-    res.status(500).send(error);
+    res.status(500).send({ error: true, teachers: user });
   }
 });
 
 router.get("/:id", async (req, res) => {
   try {
-    const user = await Class.find().lean().exec();
+    const user = await Class.find({ teacher_id: req.params.id }).lean().exec();
 
     console.log("Getting all documents of Classes");
 
-    res.status(200).send(user);
+    res.status(200).send({ error: false, teachers: user });
   } catch (error) {
     console.log("ERROR:", error);
-    res.status(500).send(error);
+    res.status(500).send({ error: true, teachers: user });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const user = await Class.findByIdAndDelete(req.params.id).lean().exec();
+
+    console.log(`${req.params.id} Class Delete`);
+
+    res.status(200).send({ error: false, teachers: "Delete" });
+  } catch (error) {
+    console.log("ERROR:", error);
+    res.status(500).send({ error: true, teachers: user });
   }
 });
 
--- src/Controllers/teacher_controller.js
+++ src/Controllers/teacher_controller.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 
 const Teacher = require("../Models/teacher_model");
 
+const Class = require("../Models/class_model");
+
 const router = express.Router();
 
 router.post("", async (req, res) => {
@@ -12,10 +14,10 @@ router.post("", async (req, res) => {
 
     console.log("Teacher added");
 
-    res.status(202).send(user);
+    res.status(202).send({ error: false, teachers: user });
   } catch (error) {
     console.log("ERROR:", error);
-    res.send(500).send(error);
+    res.send(500).send({ error: true, teachers: "" });
   }
 });
 
@@ -25,10 +27,26 @@ router.get("", async (req, res) => {
 
     console.log("Showing all documents of teacher");
 
-    res.status(200).send(user);
+    res.status(200).send({ error: false, teachers: user });
   } catch (error) {
     console.log("ERROR:", error);
-    res.status(500).send(error);
+    res.status(500).send({ error: true, teachers: "" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const user = await Teacher.findByIdAndDelete(req.params.id).lean().exec();
+    const user_class = await Class.deleteMany({ teacher_id: req.params.id })
+      .lean()
+      .exec();
+
+    console.log(`${req.params.id} teacher deleted`);
+
+    res.status(200).send({ error: false, teachers: "Delete" });
+  } catch (error) {
+    console.log();
+    res.status(500).send({ error: true, teachers: "" });
   }
 });
 
